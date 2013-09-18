4eca2a81e350028fc4aba4da7b95916569276c84
Sinta Fergus <sintafergus@gmail.com>
2013-09-18 14:19:35 +0000

    Shapes notes

---
+++ Shapes-Sep12.R
@@ -0,0 +1,21 @@
+# Shapes
+
+plot(1:100)
+plot(1:100,type="n")
+text(40, 20, "Hello World", col="blue")
+
+plot (1:100)
+
+plot(1:100,type="n")
+text(40, 20, "Hello World", col="blue")
+
+polygon(c(45,65,65,45),c(45,45,65,65),col="tomato")
+
+polygon(c(45,65,65,45),c(45,45,65,65),col="tomato",border=NA)
+
+polygon(c(45,65,65,45),c(45,45,65,65),col="tomato",border=NA)
+
+polygon(angles<-0:(2*pi))
+
+polygon(seq(0,(2*pi),length=1000))
+
